3859504e8cc43b1d8eec2897752b93f0b8195d94
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-25 13:50:01 +0000

    Unit7-05:Ands, ORs Staements

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('button').addEventListener('click', input)
+let age = 0
+let day = ''
+
+function input () {
+  age = document.getElementById('age-input').value
+  age = parseInt(age)
+  day = document.getElementById('day-input').value
+  
+  if (age <= 18 && (day != 'saturday' && day != 'sunday')) {
+    alert('Have Fun at School!')
+  } else if (age > 18 && (day != 'saturday' && day != 'sunday')) {
+    alert('Have Fun at Work!')
+  }
+  else {
+    alert('Nice Weekend!')
+  }
+}
